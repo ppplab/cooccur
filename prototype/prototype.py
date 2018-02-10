24fbe540bb500c88db7e3da0eb631d6fc734a47e
Adam Novak <anovak@soe.ucsc.edu>
2018-02-09 16:08:55 -0800

    Bring PPP into cooccurrence prototype

--- prototype/prototype.py
+++ prototype/prototype.py
@@ -14,6 +14,10 @@ import sys
 import random
 
 import collections
+import itertools
+
+import numpy
+import ppp.prototype
 
 def parse_args(args):
     """
@@ -33,6 +37,11 @@ def parse_args(args):
     parser = argparse.ArgumentParser(description=__doc__, 
         formatter_class=argparse.RawDescriptionHelpFormatter)
     
+    parser.add_argument("--full-scale", action="store_true",
+        help="run at full scale gene/pop size instead of small test size")
+    parser.add_argument("--epsilon", type=float, default=1.0,
+        help="privacy preserving factor")
+        
     # The command line arguments start with the program name, which we don't
     # want to treat as an argument for argparse. So we remove it.
     args = args[1:]
@@ -148,7 +157,7 @@ def has_pathogenic_in_trans(haplotype_pair, database):
             
     return pathogenic1 and pathogenic2
 
-def make_next_generation(population, references, databases, pop_size, p_mutation = 0.01):
+def make_next_generation(population, references, databases, pop_size, p_mutation = 0.001):
     """
     Make the next generation population from the previous generation
     population.  Also takes a list of reference strings for all genes, and a
@@ -351,7 +360,42 @@ def report_genotypes(population, databases):
             
         print("Gene {}: {}/{} individuals carry a pathogenic variant".format(gene, pathogenic_count, len(population)))
                 
-        
+def make_cooccurrence_lists(population):
+    """
+    For each gene, output a list, per individual, of all cooccurring pairs of
+    variants.
+    """
+    
+    # Make a list for each gene of lists for each person of cooccurring variant pairs
+    cooccurring = [[] for gene in population[0]]
+    
+    for individual in population:
+        for gene_num, gene_copies in enumerate(individual):
+            # Make a list of cooccurring pairs for this person
+            cooccurring_for_person = []
+            
+            # Compose one big set of all the variants in this person's
+            # haplotypes for this gene
+            all_variants = set()
+            for haplotype in gene_copies:
+                for variant in haplotype:
+                    # Add each variant the person has in the gene to the set
+                    all_variants.add(variant)
+                    
+            for var1, var2 in itertools.combinations(sorted(all_variants), 2):
+                # For each pair of distinct variants in cannonical order
+                
+                # Record a cooccurrence
+                cooccurring_for_person.append((var1, var2))
+                
+            # Save the person;s results for this gene
+            cooccurring[gene_num].append(cooccurring_for_person)
+    
+    return cooccurring
+                
+                
+            
+                
 
 def main(args):
     """
@@ -362,8 +406,14 @@ def main(args):
     
     options = parse_args(args) # This holds the nicely-parsed options object
     
-    # Run the simulation
-    population, references, databases = evolve_population([125951, 85405], 30000, 1000)
+    if options.full_scale:
+        # Run the simulation at real scale
+        # TODO: this takes an hour and we really should cache the simulated data
+        population, references, databases = evolve_population([125951, 85405], 30000, 1000)
+    else:
+        # Run the simulation at a smaller scale
+        population, references, databases = evolve_population([100], 300, 1000)
+    
     
     # Report the final variant frequencies
     report_frequencies(population, references, databases)
@@ -371,9 +421,40 @@ def main(args):
     # And the overall summary statistics
     report_genotypes(population, databases)
     
-    # TODO: Construct a censored version of databases, duplicate population
-    # members to fill out the number we need for co-occurrence analysis, and
-    # try to process co-occurrence data in a privacy-preserving way.
+    # Construct the big cooccurrence lists of all cooccurrence instances, by gene
+    cooccurring = make_cooccurrence_lists(population)
+    
+    # Make a PPP thing to privacy-preserve the data
+    preserver = ppp.prototype.Prototype(options.epsilon)
+    
+    for gene_num, gene_cooccurrences in enumerate(cooccurring):
+        # For each gene and the data for all the people for that gene
+        print("Computing histograms for Gene {}".format(gene_num))
+        
+        # Compute a histogram with a counter to make sure we understand the PPP histograms
+        cooccur_counts = collections.Counter()
+        for person in gene_cooccurrences:
+            for cooccurrence in person:
+                cooccur_counts[cooccurrence] += 1
+        print cooccur_counts
+        
+        # Flatten cooccurrences all into one list.
+        # Also reduce tuples to strings because PPP thinks the data is the wrong shape and the tuples aren't symbols
+        # TODO: Make PPP understand multiple data items per participant and tuples as data items
+        cooccurrences_flat = [str(co) for person in gene_cooccurrences for co in person]
+        
+        # Compute true and private histograms
+        true_histogram = preserver.get_histogram(cooccurrences_flat, False)
+        private_histogram = preserver.get_histogram(cooccurrences_flat, True)
+        
+        # Make into 2d arrays
+        # TODO: Require numpy 1.10 and stack
+        true_histogram = numpy.reshape(true_histogram, (len(true_histogram), 1))
+        private_histogram = numpy.reshape(private_histogram, (len(private_histogram), 1)) 
+        
+        # Compare side by side
+        both_histograms = numpy.concatenate((true_histogram, private_histogram), axis=1)
+        print(both_histograms)
     
 if __name__ == "__main__" :
     # Provide main with its arguments and handle exit codes
